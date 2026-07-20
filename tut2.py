9160fe48cfefe9071c89b2fad4b20d139d35e0db
TheDeveloper69k <hitenkashyap20@gmail.com>
2026-07-20 21:01:35 +0530

    tut2

--- tut2.py
+++ tut2.py
@@ -1,4 +1,6 @@
-#ARITHMETIC OPERATIONS
+#OPERATORS IN PYTHON
+
+#ARITHMETIC OPERATORS
 
 a = 10
 b = 5
@@ -7,3 +9,32 @@ print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
 print("Division:", a / b)
+
+#ASSIGNMENT OPERATORS
+
+c = 10
+c += 5
+
+print("Value of d after addition:", c)
+
+d = 10
+d -= 5
+
+print("Value of d after subtraction:", d)
+
+#COMPARISON OPERATORS
+
+e = 10
+f = 5
+
+print("Is e equal to f?", e == f)
+print("Is e not equal to f?", e != f)   
+
+#Logical Operators
+
+g = True
+h = False
+
+print("Logical AND:", g and h)
+print("Logical OR:", g or h)
+print("Logical NOT:", not g)
